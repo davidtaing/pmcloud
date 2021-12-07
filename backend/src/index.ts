f965705966b3bc8d0a651e3684b68e78ed4bcf28
David Taing <adavidtaing@gmail.com>
2021-12-07 14:27:46 +1100

    refactor: wrap main block inside async function
    to ensure that mongoose connects to the db first before express init.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,29 +7,33 @@ import mongoose from "mongoose";
 import { MongoUrl } from "./config";
 import router from "./routes";
 
-mongoose
-  .connect(MongoUrl)
-  .then(() => console.log("Successfully connected to MongoDB"))
-  .catch((err) => console.error(err));
-
 const app = express();
 const port = 3000;
 
-app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+async function main() {
+  await mongoose
+    .connect(MongoUrl)
+    .then(() => console.log("Successfully connected to MongoDB"))
+    .catch((err) => console.error(err));
 
-app.use(mongoSanitize());
-app.use(compression());
+  app.use(helmet());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-// Routers
-app.use("/v1", router);
+  app.use(mongoSanitize());
+  app.use(compression());
 
-// Error Handler
-app.use((req, res, next) => {
-  res.status(500).json({ message: "Internal Server Error" });
-});
+  // Routers
+  app.use("/v1", router);
 
-app.listen(port, () => {
-  console.log(`Server is listening on ${port}`);
-});
+  // Error Handler
+  app.use((req, res, next) => {
+    res.status(500).json({ message: "Internal Server Error" });
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is listening on ${port}`);
+  });
+}
+
+main();
