b50efb25e482dbdfaa64c736ba74cb724f4c6902
David Taing <adavidtaing@gmail.com>
2021-12-07 13:34:30 +1100

    feat: v1/properties create response type

--- backend/src/routes/properties/index.ts
+++ backend/src/routes/properties/index.ts
@@ -1,29 +1,40 @@
 import express from "express";
+import Landlord from "../../types/Landlord";
+import Tenant from "../../types/Tenant";
 
 const router = express.Router();
 
-const data = {
-  addressLn1: "52 Ocean St",
-  addressLn2: "Sydney NSW 2000",
+type response = {
+  addressLn1: string;
+  addressLn2: string;
   contactDetails: {
-    landlord: {
-      fullName: "David Taing",
-      phone: "0255500000",
-      mobile: "0491570006",
-      email: "davidtaing@email.com",
-      addressLn1: "50 Ocean St",
-      addressLn2: "Sydney NSW 2000",
-    },
-    tenant: {
-      fullName: "Taing David",
-      phone: "",
-      mobile: "0491570156",
-      email: "taingdavid@email.com",
-    },
-  },
+    landlord: Landlord;
+    tenant: Tenant;
+  };
 };
 
 router.route("/").get((req, res, err) => {
+  const data: response = {
+    addressLn1: "52 Ocean St",
+    addressLn2: "Sydney NSW 2000",
+    contactDetails: {
+      landlord: {
+        fullName: "David Taing",
+        phone: "0255500000",
+        mobile: "0491570006",
+        email: "davidtaing@email.com",
+        addressLn1: "50 Ocean St",
+        addressLn2: "Sydney NSW 2000",
+      },
+      tenant: {
+        fullName: "Taing David",
+        phone: "",
+        mobile: "0491570156",
+        email: "taingdavid@email.com",
+      },
+    },
+  };
+
   console.log(data);
   res.json(data);
 });
