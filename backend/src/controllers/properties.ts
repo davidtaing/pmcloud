b20f9cb18bab0b4ec225b46502cf650c9b489d9b
David Taing <adavidtaing@gmail.com>
2021-12-07 15:27:24 +1100

    feat: getProperty controller now gets data from mongo

--- backend/src/controllers/properties.ts
+++ backend/src/controllers/properties.ts
@@ -1,37 +1,10 @@
-import Landlord from "../types/Landlord";
-import Tenant from "../types/Tenant";
+import mongoose from "mongoose";
+import PropertySchema from "../models/PropertySchema";
 
-type response = {
-  addressLn1: string;
-  addressLn2: string;
-  contactDetails: {
-    landlord: Landlord;
-    tenant: Tenant;
-  };
-};
+export const getProperty = async (req: any, res: any, err: any) => {
+  const propertyModel = mongoose.model("property", PropertySchema);
+  const property = await propertyModel.find();
 
-export const getProperty = (req: any, res: any, err: any) => {
-  const data: response = {
-    addressLn1: "52 Ocean St",
-    addressLn2: "Sydney NSW 2000",
-    contactDetails: {
-      landlord: {
-        fullName: "David Taing",
-        phone: "0255500000",
-        mobile: "0491570006",
-        email: "davidtaing@email.com",
-        addressLn1: "50 Ocean St",
-        addressLn2: "Sydney NSW 2000",
-      },
-      tenant: {
-        fullName: "Taing David",
-        phone: "",
-        mobile: "0491570156",
-        email: "taingdavid@email.com",
-      },
-    },
-  };
-
-  console.log(data);
-  res.json(data);
+  console.log(property);
+  res.json(property);
 };
