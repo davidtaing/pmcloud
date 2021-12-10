a540c66b3e6ef79be0f2220cc2a87918584c7fc7
David Taing <adavidtaing@gmail.com>
2021-12-10 18:04:56 +1100

    docs: add comments to route handlers

--- backend/src/api/v1/paths/index.ts
+++ backend/src/api/v1/paths/index.ts
@@ -1,10 +1,12 @@
 import { paths } from "../api-doc";
 import { Operation } from "express-openapi";
 
+// Get Landlords
 const GET: Operation = (req: any, res: any, next: any) => {
   res.status(501).json({ message: "Not Yet Implemented" });
 };
 
+// Create Landlord
 const POST: Operation = (req: any, res: any, next: any) => {
   res.status(501).json({ message: "Not Yet Implemented" });
 };
--- backend/src/api/v1/paths/{landlordId}.ts
+++ backend/src/api/v1/paths/{landlordId}.ts
@@ -1,10 +1,12 @@
 import { paths } from "../api-doc";
 import { Operation } from "express-openapi";
 
+// Get Landlord
 const GET: Operation = (req: any, res: any, next: any) => {
   res.status(501).json({ message: "Not Yet Implemented" });
 };
 
+// Update Landlord
 const PATCH: Operation = (req: any, res: any, next: any) => {
   res.status(501).json({ message: "Not Yet Implemented" });
 };
