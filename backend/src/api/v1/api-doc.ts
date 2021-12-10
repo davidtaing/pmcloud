24efd3552102364abfbcdc745b4635ff1e1c34a4
David Taing <adavidtaing@gmail.com>
2021-12-10 14:54:35 +1100

    refactor: paths object out of apiDoc

--- backend/src/api/v1/api-doc.ts
+++ backend/src/api/v1/api-doc.ts
@@ -1,3 +1,7 @@
-import apiDoc from "./api.json";
+import apiSpec from "./api.json";
+
+const { paths: pathsSpec, ...otherSpecs } = apiSpec;
+const apiDoc = { paths: {}, ...otherSpecs };
+export const paths = pathsSpec;
 
 export default apiDoc;
