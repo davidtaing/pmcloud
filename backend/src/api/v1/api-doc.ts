d4183ecae790e6d9ab510857fd81c0ba3576e726
David Taing <adavidtaing@gmail.com>
2021-12-10 18:53:27 +1100

    feat: add openapi-types

--- backend/src/api/v1/api-doc.ts
+++ backend/src/api/v1/api-doc.ts
@@ -1,7 +1,8 @@
 import apiSpec from "./api.json";
+import { OpenAPIV3 } from "openapi-types";
 
 const { paths: pathsSpec, ...otherSpecs } = apiSpec;
-const apiDoc = { paths: {}, ...otherSpecs };
-export const paths = pathsSpec;
+const apiDoc = { paths: {}, ...otherSpecs } as OpenAPIV3.Document;
+export const paths = pathsSpec as OpenAPIV3.PathsObject;
 
 export default apiDoc;
