e68135a4b6e56f514c4f9cf5aa3200fc4bfca7b9
David Taing <adavidtaing@gmail.com>
2021-12-10 19:43:46 +1100

    docs: throw error on failed openapi load

--- backend/src/api/v1/api-doc.ts
+++ backend/src/api/v1/api-doc.ts
@@ -5,4 +5,7 @@ const { paths: pathsSpec, ...otherSpecs } = apiSpec;
 const apiDoc = { paths: {}, ...otherSpecs } as OpenAPIV3.Document;
 export const paths = pathsSpec as OpenAPIV3.PathsObject;
 
+if (apiDoc === undefined || paths === undefined)
+  throw Error("Failed to load OpenAPI Spec.");
+
 export default apiDoc;
