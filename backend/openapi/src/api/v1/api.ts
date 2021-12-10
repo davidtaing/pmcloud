be3036fc2ba123f3ac0c4a95205a5c44fe7be613
David Taing <adavidtaing@gmail.com>
2021-12-10 12:20:45 +1100

    feat: add version metadata to openapi generator

--- backend/openapi/src/api/v1/api.ts
+++ backend/openapi/src/api/v1/api.ts
@@ -2,5 +2,8 @@ import { api } from "@airtasker/spot";
 
 import "./landlords";
 
-@api({ name: "pmcloud Backend API" })
+@api({
+  name: "pmcloud Backend API",
+  version: "1.0.0",
+})
 class Api {}
