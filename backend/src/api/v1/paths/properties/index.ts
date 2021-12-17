27e4916bb586be4833c93e286f3974db0a5443da
David Taing <adavidtaing@gmail.com>
2021-12-17 15:28:13 +1100

    fix: Unexpected Token SyntaxError

--- backend/src/api/v1/paths/properties/index.ts
+++ backend/src/api/v1/paths/properties/index.ts
@@ -3,16 +3,12 @@ import { paths } from "../../api-doc";
 import { Operation } from "express-openapi";
 
 // Get Properties
-const GET: Operation = (req: Request,
-  res: Response,
-  next: NextFunction) => {
+const GET: Operation = (req: Request, res: Response, next: NextFunction) => {
   res.status(501).json({ message: "Not Yet Implemented" });
 };
 
 // Create Property
-const POST: Operation = (req: Request,
-  res: Response,
-  next: NextFunction => {
+const POST: Operation = (req: Request, res: Response, next: NextFunction) => {
   res.status(501).json({ message: "Not Yet Implemented" });
 };
 
