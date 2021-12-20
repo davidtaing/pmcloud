8ce02446ac8bc4ba7e4d589b02b8fd49e221a6b0
David Taing <adavidtaing@gmail.com>
2021-12-21 10:59:32 +1100

    test:  add tests for CreateLandlord

--- backend/src/test/integration-tests/landlords/landlords.service.test.ts
+++ backend/src/test/integration-tests/landlords/landlords.service.test.ts
@@ -1,14 +1,14 @@
 import {
+  connectTestDatabase,
+  clearTestDatabase,
+  closeTestDatabase,
+} from "../../../testDb";
+import {
   GetLandlord,
   GetLandlords,
   CreateLandlord,
   UpdateLandlord,
 } from "../../../api/v1/services/landlords.service";
-import {
-  connectTestDatabase,
-  clearTestDatabase,
-  closeTestDatabase,
-} from "../../../testDb";
 
 beforeAll(async () => {
   await connectTestDatabase();
@@ -28,6 +28,34 @@ describe("Landlords Service", () => {
       const result = await GetLandlords();
 
       expect(result).toHaveLength(0);
+    });
+  });
+
+  describe("CreateLandlord", () => {
+    let payload: any;
+    let result: any;
+
+    beforeAll(async () => {
+      payload = {
+        fullname: "John Smith",
+        mobile: "0491570006",
+        email: "davidtaing@fake.com",
+        addressLn1: "123 Fake St",
+        addressLn2: "Sydney NSW 2000",
+      };
+      result = await CreateLandlord(payload);
+    });
+
+    it("should return new landlord object with the same payload content", () => {
+      expect(result).toEqual(expect.objectContaining(payload));
+    });
+
+    it("should have a __v property added", () => {
+      expect(result).toHaveProperty("__v");
+    });
+
+    it("should have a _id property added", () => {
+      expect(result).toHaveProperty("_id");
     });
   });
 });
