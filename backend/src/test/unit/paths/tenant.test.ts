d93e095791f04ec2fcc5106d2cc989156cf9b094
David Taing <adavidtaing@gmail.com>
2021-12-21 21:03:06 +1100

    test: path method tests

---
+++ backend/src/test/unit/paths/tenant.test.ts
@@ -0,0 +1,30 @@
+import operations from "../../../api/v1/paths/tenants";
+
+const methods = operations();
+
+/**
+ * @group unit
+ * Will break on changes to the API
+ * i.e. when new methods are added to /tenants path
+ */
+describe("/tenants Path Object", () => {
+  it("should have GET method", () => {
+    expect(methods).toHaveProperty("GET");
+  });
+
+  it("should have POST method", () => {
+    expect(methods).toHaveProperty("POST");
+  });
+
+  it("should not have DELETE method", () => {
+    expect(methods).not.toHaveProperty("DELETE");
+  });
+
+  it("should not have PUT method", () => {
+    expect(methods).not.toHaveProperty("PUT");
+  });
+
+  it("should not have PATCH method", () => {
+    expect(methods).not.toHaveProperty("PATCH");
+  });
+});
---
+++ backend/src/test/unit/paths/{propertyId}.test.ts
@@ -0,0 +1,30 @@
+import operations from "../../../api/v1/paths/properties/{propertyId}";
+
+const methods = operations();
+
+/**
+ * @group unit
+ * Will break on changes to the API
+ * i.e. when new methods are added to /properties/{propertyId} path
+ */
+describe("/properties/{propertyId} Path Object", () => {
+  it("should have GET method", () => {
+    expect(methods).toHaveProperty("GET");
+  });
+
+  it("should have PATCH method", () => {
+    expect(methods).toHaveProperty("PATCH");
+  });
+
+  it("should not have POST method", () => {
+    expect(methods).not.toHaveProperty("POST");
+  });
+
+  it("should not have DELETE method", () => {
+    expect(methods).not.toHaveProperty("DELETE");
+  });
+
+  it("should not have PUT method", () => {
+    expect(methods).not.toHaveProperty("PUT");
+  });
+});
