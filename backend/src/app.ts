f0fac7589f876ab9f876ffd04498c7480f6bae3e
David Taing <adavidtaing@gmail.com>
2021-12-21 19:21:44 +1100

    refactor: express app to separate file
    in prepartion for supertest integration tests

---
+++ backend/src/app.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import helmet from "helmet";
+import mongoSanitize from "express-mongo-sanitize";
+import compression from "compression";
+import { initialize } from "express-openapi";
+import swaggerUi from "swagger-ui-express";
+
+import apiDoc from "./api/v1/api-doc";
+
+const app = express();
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(mongoSanitize());
+app.use(compression());
+
+initialize({
+  apiDoc: apiDoc,
+  app,
+  paths: "./src/api/v1/paths",
+  routesGlob: "**/*.{ts,js}",
+  routesIndexFileRegExp: /(?:index)?\.[tj]s$/,
+});
+
+app.use(
+  "/openapi",
+  swaggerUi.serve,
+  swaggerUi.setup(undefined, {
+    swaggerOptions: {
+      url: "http://localhost:3000/api-docs",
+    },
+  })
+);
+
+app.use(((err, req, res, next) => {
+  res.status(err.status).json(err);
+}) as express.ErrorRequestHandler);
+
+export default app;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,51 +1,13 @@
-import express from "express";
-import helmet from "helmet";
-import mongoSanitize from "express-mongo-sanitize";
-import compression from "compression";
-import { initialize } from "express-openapi";
-import swaggerUi from "swagger-ui-express";
-
-import apiDoc from "./api/v1/api-doc";
+import app from "./app";
 import connectDb from "./db";
+const PORT = 3000;
 
-const app = express();
-const port = 3000;
-
-async function main() {
-  connectDb();
-
-  app.use(helmet());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-
-  app.use(mongoSanitize());
-  app.use(compression());
-
-  initialize({
-    apiDoc: apiDoc,
-    app,
-    paths: "./src/api/v1/paths",
-    routesGlob: "**/*.{ts,js}",
-    routesIndexFileRegExp: /(?:index)?\.[tj]s$/,
-  });
-
-  app.use(
-    "/openapi",
-    swaggerUi.serve,
-    swaggerUi.setup(undefined, {
-      swaggerOptions: {
-        url: "http://localhost:3000/api-docs",
-      },
-    })
-  );
-
-  app.use(((err, req, res, next) => {
-    res.status(err.status).json(err);
-  }) as express.ErrorRequestHandler);
-
-  app.listen(port, () => {
-    console.log(`Server is listening on ${port}`);
-  });
+async function connectDatabase() {
+  await connectDb();
 }
 
-main();
+connectDatabase();
+
+app.listen(PORT, () => {
+  console.log(`Server Listening on Port ${PORT}`);
+});
