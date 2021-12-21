27ed33366337bf71f6546be10e00eddbb6134a3d
David Taing <adavidtaing@gmail.com>
2021-12-22 10:46:08 +1100

    changed urlencoded extended to false

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(helmet());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(mongoSanitize());
 app.use(compression());
