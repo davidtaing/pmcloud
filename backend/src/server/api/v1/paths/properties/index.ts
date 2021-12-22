21aea80472737d855d1896bda4b7fe7cb009ecdf
David Taing <adavidtaing@gmail.com>
2021-12-22 18:14:58 +1100

    init: zod and add zod schema files

--- backend/src/server/api/v1/paths/properties/index.ts
+++ backend/src/server/api/v1/paths/properties/index.ts
@@ -1,19 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 import { paths } from "../../api-doc";
 import { Operation } from "express-openapi";
-import {
-  getProperties,
-  createProperty,
-} from "../../controllers/properties.controller";
 
 // Get Properties
 const GET: Operation = (req: Request, res: Response, next: NextFunction) => {
-  getProperties(req, res, next);
+  res.status(501).json({ message: "Not Yet Implemented" });
 };
 
 // Create Property
 const POST: Operation = (req: Request, res: Response, next: NextFunction) => {
-  createProperty(req, res, next);
+  res.status(501).json({ message: "Not Yet Implemented" });
 };
 
 GET.apiDoc = paths["/properties"]?.get;
--- backend/src/server/api/v1/paths/properties/{propertyId}.ts
+++ backend/src/server/api/v1/paths/properties/{propertyId}.ts
@@ -1,19 +1,15 @@
 import { Request, Response, NextFunction } from "express";
 import { paths } from "../../api-doc";
 import { Operation } from "express-openapi";
-import {
-  getPropertyById,
-  updateProperty,
-} from "../../controllers/properties.controller";
 
 // Get Property
 const GET: Operation = (req: Request, res: Response, next: NextFunction) => {
-  getPropertyById(req, res, next);
+  res.status(501).json({ message: "Not Yet Implemented" });
 };
 
 // Update Property
 const PATCH: Operation = (req: Request, res: Response, next: NextFunction) => {
-  updateProperty(req, res, next);
+  res.status(501).json({ message: "Not Yet Implemented" });
 };
 
 GET.apiDoc = paths["/properties/{propertyId}"]?.get;
