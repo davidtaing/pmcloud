32a9749785590b96c3238058edc91cfaed31f9e6
David Taing <adavidtaing@gmail.com>
2021-12-22 11:11:20 +1100

    test: POST /landlords

--- backend/src/test/integration/landlords/landlords.controller.test.ts
+++ backend/src/test/integration/landlords/landlords.controller.test.ts
@@ -6,6 +6,14 @@ import {
   closeTestDatabase,
 } from "../../util/testDb";
 
+const testLandlord = {
+  fullname: "John Smith",
+  mobile: "0491570006",
+  email: "davidtaing@fake.com",
+  addressLn1: "123 Fake St",
+  addressLn2: "Sydney NSW 2000",
+};
+
 beforeAll(async () => {
   await connectTestDatabase();
 });
@@ -30,13 +38,50 @@ describe("/landlords controllers", () => {
   });
 
   describe("POST /landlords", () => {
-    it("should have 201 status code", async () => {
-      const res = await request(app)
-        .post("/landlords")
-        .send()
-        .set("Accept", "application/json");
+    describe("Successful Response", () => {
+      it("should have 201 status code", async () => {
+        const res = await request(app)
+          .post("/landlords")
+          .send(testLandlord)
+          .set("Accept", "application/json");
 
-      expect(res.statusCode).toBe(201);
+        expect(res.statusCode).toBe(201);
+      });
+    });
+
+    describe("Empty Landlord Object", () => {
+      it("should have 400 status code", async () => {
+        const res = await request(app)
+          .post("/landlords")
+          .send({})
+          .set("Accept", "application/json");
+
+        expect(res.statusCode).toBe(400);
+      });
+    });
+
+    describe("Missing Email Prop", () => {
+      it("should have 400 status code", async () => {
+        const { email, ...otherLandlordProps } = testLandlord;
+
+        const res = await request(app)
+          .post("/landlords")
+          .send({ ...otherLandlordProps })
+          .set("Accept", "application/json");
+
+        expect(res.statusCode).toBe(400);
+      });
+    });
+
+    describe("Empty Email Prop", () => {
+      it("should have 400 status code", async () => {
+        const res = await request(app)
+          .post("/landlords")
+          .send({ ...testLandlord, email: "" })
+          .set("Accept", "application/json");
+
+        expect(res.statusCode).toBe(400);
+      });
     });
   });
 });
