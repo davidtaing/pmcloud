64cd4210e387e195a939e8bd06794df5dee01ea0
David Taing <adavidtaing@gmail.com>
2021-12-31 13:26:10 +1100

    test: post /landlords 201 response

--- backend/src/server/test/integration/landlords/landlords.routes.test.ts
+++ backend/src/server/test/integration/landlords/landlords.routes.test.ts
@@ -3,9 +3,9 @@ import request from "supertest";
 import TestDb from "../../util/testDb";
 
 const testLandlord = {
-  fullname: "John Smith",
+  fullname: "Test Smith",
   mobile: "0491570006",
-  email: "davidtaing@fake.com",
+  email: "testsmith@fake.com",
   address: {
     addressLn1: "123 Fake St",
     addressLn2: "Sydney NSW 2000",
@@ -23,7 +23,25 @@ afterAll(async () => {
 
 describe("/landlords", () => {
   describe("POST", () => {
-    test.todo("200 - OK");
+    describe("201 - CREATED", () => {
+      let res: any;
+
+      beforeAll(async () => {
+        res = await request(app)
+          .post("/landlords")
+          .set("Accept", "application/json")
+          .send(testLandlord);
+      });
+
+      test("status is 201", () => {
+        expect(res.status).toBe(201);
+      });
+
+      test("response body matches testLandlord", () => {
+        expect(res.body).toMatchObject(testLandlord);
+      });
+    });
+
     test.todo("400 - Bad Request");
     test.todo("403 - Forbidden");
     test.todo("500 - Internal Server Error");
